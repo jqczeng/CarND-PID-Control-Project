d0b95eb4baede25ac9f423d86b855b9d83f77264
Jason Zeng <jzeng@Jasons-MacBook-Pro.local>
2019-05-22 09:37:36 -0400

    initial project folder of the PID project

--- src/PID.cpp
+++ src/PID.cpp
@@ -12,6 +12,9 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
 
 }
 
@@ -19,6 +22,9 @@ void PID::UpdateError(double cte) {
   /**
    * TODO: Update PID errors based on cte.
    */
+  p_error = cte;
+  i_error += cte;
+  d_error = cte - prev_cte;
 
 }
 
@@ -26,5 +32,7 @@ double PID::TotalError() {
   /**
    * TODO: Calculate and return the total error
    */
-  return 0.0;  // TODO: Add your total error calc here!
+
+
+  return p_error + i_error + d_error;  // TODO: Add your total error calc here!
 }
